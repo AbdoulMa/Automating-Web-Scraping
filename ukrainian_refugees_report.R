f24400ffc4552c106d1c6979d9e9a82fb0f0c733
AbdoulMa <issamadjid1995@gmail.com>
2022-03-05 18:39:13 +0100

    Ukranian refugees daily data sraping.

---
+++ ukrainian_refugees_report.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+
+# Data file path 
+overall_refugees_file <- "data/overall_refugees.csv"
+
+# Retrieve daily data published by UNHCR 
+daily_overall_refugees <- rjson::fromJSON(file = "https://data2.unhcr.org/population/get/sublocation?geo_id=0&forcesublocation=1&widget_id=283557&sv_id=54&color=%233c8dbc&color2=%23303030&population_group=5460") %>% 
+  pluck("data") %>% 
+  tibble::enframe() %>% 
+  unnest_wider(value) %>% 
+  select(-where(is.list)) %>% 
+  relocate(c("date", "individuals"), .after =  "admin_level") %>% 
+  mutate(date = readr::parse_date(date)) 
+
+# Retrieve former data 
+if (fs::file_exists(overall_refugees_file)) {
+  overall_refugees <- readr::read_csv(overall_refugees_file)
+} else {
+  overall_refugees <- daily_overall_refugees
+}
+
+# Check if new daily data 
+daily_refugees <- daily_overall_refugees %>%
+  anti_join(overall_refugees, by = c("geomaster_id", "date"))
+
+if (nrow(daily_refugees)) {
+  write_csv(daily_refugees, here::here("data",paste0(format(lubridate::today(), "%Y_%m_%d"),".csv")))
+  bind_rows(overall_refugees, daily_refugees)
+}
+
+# Update data file 
+write_csv(overall_refugees, "data/overall_refugees.csv")
+
